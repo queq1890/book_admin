3e2e80ce32fafd3dd083ad8705850a3506b72cf0
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-11 21:53:34 +0900

    Improve rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-    get 'books/:id' => 'books#show'
-    resources :publishers
+  resources :publishers do
+    resources :books
+
+    member do
+      get 'detail'
+    end
+
+    collections do
+      get 'search'
+    end
+  end
 end
