9b5977a3144830ad19bc2af8a947dbcb7c335577
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-11 22:52:03 +0900

    UserAgentがiPhoneだった場合の記述を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  begore_action :detect_mobile_variant
   protect_from_forgery with: :exception
+
+  private
+
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
+  end
 end
