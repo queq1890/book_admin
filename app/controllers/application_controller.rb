4114cc5afc344be57f61572387b22da61c5d7935
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-11 22:55:28 +0900

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  begore_action :detect_mobile_variant
+  before_action :detect_mobile_variant
   protect_from_forgery with: :exception
 
   private
